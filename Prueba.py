fc911d4c10a0a4b9e4ef85c01feece9e58271477
Marru73 <marru973@gmail.com>
2024-08-13 13:35:52 +0200

    prueba

--- Prueba.py
+++ Prueba.py
@@ -1,8 +1,42 @@
-def suma(a, b):
-    sumar = a + b
-    print(sumar)
+import os
+"""file_name = "Marru.txt"
+with open(file_name, "w") as file:
+    file.write("Fran\n")
+    file.write("51\n")
+    file.write("Python")
 
-a = int(input("Introduce el primer número: "))
-b = int(input("Introduce el segundo número: "))
+with open(file_name, "r") as file:
+    file.read()
+    print(file.read())
 
-suma(a, b)
+os.remove(file_name)"""
+
+file_name = "Ventas.txt"
+
+with open(file_name, "a") as file:
+    while True:
+        print("1. Añadir producto")
+        print("2. Consultar producto")
+        print("3. Actualizar producto")
+        print("4. Borrar producto")
+        print("5. Mostrar productos")
+        print("6. Calcular venta total")
+        print("7. Calcular venta por producto")
+        print("8. Salir")
+
+        option = input("Selecciona una opción: ")
+
+        if option == 1:
+            name = input("Introduce el nombre: ")
+            quentity = input("Introduce la cantidad: ")
+            price = input("Introduce el precio: ")
+
+        elif option == 2:
+            name = input("Introduce el nombre a consultar: ")
+            with open(file_name, "r") as file:
+                file.read()
+                print(file.read())
+
+
+
+  
